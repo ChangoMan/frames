6985affbb56b272238deb3ba372af8c249f8de26
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-03-01 18:04:45 +0800

    adding advanced example

---
+++ src/app/advanced/page.tsx
@@ -0,0 +1,48 @@
+import { getFrameMetadata } from '@coinbase/onchainkit/frame'
+import type { Metadata } from 'next'
+
+const frameMetadata = getFrameMetadata({
+  buttons: [
+    {
+      label: 'Story time!',
+    },
+    {
+      action: 'link',
+      label: 'Link to Google',
+      target: 'https://www.google.com',
+    },
+    {
+      label: 'Redirect to pictures',
+      action: 'post_redirect',
+    },
+  ],
+  image: {
+    src: `${process.env.NEXT_PUBLIC_SITE_URL}/park-3.png`,
+    aspectRatio: '1:1',
+  },
+  input: {
+    text: 'Tell me a boat story',
+  },
+  postUrl: `${process.env.NEXT_PUBLIC_SITE_URL}/api/frame`,
+})
+
+export const metadata: Metadata = {
+  title: 'zizzamia.xyz',
+  description: 'LFG',
+  openGraph: {
+    title: 'zizzamia.xyz',
+    description: 'LFG',
+    images: [`${process.env.NEXT_PUBLIC_SITE_URL}/park-1.png`],
+  },
+  other: {
+    ...frameMetadata,
+  },
+}
+
+export default function Page() {
+  return (
+    <>
+      <h1>Advanced Frame</h1>
+    </>
+  )
+}
---
+++ src/app/api/advanced/route.ts
@@ -0,0 +1,51 @@
+import {
+  FrameRequest,
+  getFrameHtmlResponse,
+  getFrameMessage,
+} from '@coinbase/onchainkit/frame'
+import { NextRequest, NextResponse } from 'next/server'
+
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  let accountAddress: string | undefined = ''
+  let text: string | undefined = ''
+
+  const body: FrameRequest = await req.json()
+  const { isValid, message } = await getFrameMessage(body)
+
+  console.log('isValid', isValid)
+
+  if (isValid) {
+    accountAddress = message.interactor.verified_accounts[0]
+  }
+
+  if (message?.input) {
+    text = message.input
+  }
+
+  if (message?.button === 3) {
+    return NextResponse.redirect(
+      'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
+      { status: 302 }
+    )
+  }
+
+  return new NextResponse(
+    getFrameHtmlResponse({
+      buttons: [
+        {
+          label: `Story: 🌲🌲`,
+        },
+      ],
+      image: {
+        src: `${process.env.NEXT_PUBLIC_SITE_URL}/park-1.png`,
+      },
+      postUrl: `${process.env.NEXT_PUBLIC_SITE_URL}/api/advanced`,
+    })
+  )
+}
+
+export async function POST(req: NextRequest): Promise<Response> {
+  return getResponse(req)
+}
+
+export const dynamic = 'force-dynamic'
