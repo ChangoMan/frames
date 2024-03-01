93e006444804f0ed3301433f87f5a8059e31ba81
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-03-01 15:29:30 +0800

    adding api route

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   other: {
     'fc:frame': 'vNext',
     'fc:frame:image': `${process.env.NEXT_PUBLIC_SITE_URL}/site-preview.jpg`,
-    'fc:frame:post_url': `${process.env.NEXT_PUBLIC_SITE_URL}/test`,
+    'fc:frame:post_url': `${process.env.NEXT_PUBLIC_SITE_URL}/api`,
     'fc:frame:button:1': 'Mint',
   },
 }
