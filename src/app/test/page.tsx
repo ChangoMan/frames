79d10fa5841ef1ce09e92c1608328d34b5f71d2a
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-03-01 16:10:41 +0800

    adding park images

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -11,8 +11,8 @@ export const metadata: Metadata = {
   other: {
     'fc:frame': 'vNext',
     'fc:frame:image': `${process.env.NEXT_PUBLIC_SITE_URL}/site-preview.jpg`,
-    'fc:frame:post_url': `${process.env.NEXT_PUBLIC_SITE_URL}/api`,
-    'fc:frame:button:1': 'Mint',
+    'fc:frame:post_url': `${process.env.NEXT_PUBLIC_SITE_URL}/api?id=1`,
+    'fc:frame:button:1': 'Start',
   },
 }
 
