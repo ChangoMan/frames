7f7720e57679315da45fbd8602564fb491074c7a
Hunter Chang <716376+ChangoMan@users.noreply.github.com>
2024-03-01 16:58:18 +0800

    rename to basic example

--- src/app/api/basic/route.ts
+++ src/app/api/basic/route.ts
@@ -2,8 +2,9 @@ import { NextRequest, NextResponse } from 'next/server'
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
   const searchParams = req.nextUrl.searchParams
-  const id: any = searchParams.get('id')
-  const idAsNumber = parseInt(id)
+  const id = searchParams.get('id')
+
+  const idAsNumber = id ? Number(id) : 1
 
   const nextId = idAsNumber + 1
 
@@ -20,15 +21,15 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     <meta property="fc:frame:button:3:action" content="post_redirect" />
     <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_SITE_URL}/api/end" />
   </head></html>`)
-  } else {
-    return new NextResponse(`<!DOCTYPE html><html><head>
+  }
+
+  return new NextResponse(`<!DOCTYPE html><html><head>
     <title>This is frame ${id}</title>
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_SITE_URL}/park-${id}.png" />
     <meta property="fc:frame:button:1" content="Next Page" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_SITE_URL}/api?id=${nextId}" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_SITE_URL}/api/basic?id=${nextId}" />
   </head></html>`)
-  }
 }
 
 export async function POST(req: NextRequest): Promise<Response> {
